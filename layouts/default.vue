566acc04b889d9330862c5cbf2600114a583d674
recito prasidha <recitogigih9@gmail.com>
2021-01-25 19:59:07 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <TheHeader/>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+  import TheHeader from '@/components/navbarComp'
+ 
+  export default {
+    components: {
+      TheHeader,
+    },
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+
+    <section class="hero is-dark is-fullheight-with-navbar">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column is-6">
+              <p class="title is-1 has-text-warning">
+                Selamat datang di <br> <b>Studentnesia</b>
+                <br>
+              </p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque a ultrices nisl. Praesent nec imperdiet dui, vitae tempor leo. Vestibulum eros justo, pulvinar eget nisl vitae, varius pellentesque lectus. Fusce maximus vulputate lorem sed rhoncus. Proin ut nulla ante. Curabitur fermentum est commodo lacus pulvinar pulvinar. Donec tellus mi, efficitur quis sodales et, bibendum non lorem. Integer ac ligula euismod, vestibulum massa a, tempus sapien. Aliquam odio arcu, vulputate eu nibh in, commodo varius magna. Sed quis mi velit.</p>
+            </div>
+            <div class="column is-6">
+              <img src="/bannerimg.svg" alt="">
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+
+</script>
+
+<style>
+
+</style>
